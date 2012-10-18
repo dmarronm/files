fa097713ffa50d712fad0635f67b96d16e2637a8
Dan <dmarronm@hotmail.com>
2012-10-17 20:43:24 -0500

    cuarto commit

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,6 +1,11 @@
 class Cliente < ActiveRecord::Base
 
  has_many :categoria
+ 
+ validates :username, :presence => true
+ validates :name, :presence => true
+ validates :password_hash, :presence => true
+ validates :password_salt, :presence => true
 
   def self.authenticate(username, password)
     cliente = find_by_username(username)
